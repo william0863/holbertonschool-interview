8959e1ec2e37f59e80edf2768fe43803f9467226
william0863 <granger.w35@gmail.com>
2024-08-30 06:15:01 +0200

    update pep8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
-You have n number of locked boxes in front of you. 
-Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 
+and each box may contain keys to the other boxes
 This is a method that determines if all the boxes can be opened.
 
 If yes, return True
 If no, return False
 """
+
+
 def canUnlockAll(boxes):
     n = len(boxes)
     opened = set([0])
