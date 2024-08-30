b2e19c5480a6376b9c55227ab0d2ed3a3e8bad37
william0863 <granger.w35@gmail.com>
2024-08-30 06:15:49 +0200

    pep8 style

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 
+Each box is numbered sequentially from 0 to n - 1
 and each box may contain keys to the other boxes
 This is a method that determines if all the boxes can be opened.
 
