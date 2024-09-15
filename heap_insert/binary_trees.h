6681e24b8fae9b7effd58dcdac020af555a9111b
william0863 <granger.w35@gmail.com>
2024-09-15 18:29:35 +0200

    task0 update

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,3 +1,8 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,3 +18,10 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+#endif
