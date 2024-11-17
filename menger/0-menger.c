2e6471b292ee9380d7cef64a712a1ff84d9440a7
william0863 <granger.w35@gmail.com>
2024-11-17 22:37:57 +0100

    menger project

---
+++ menger/0-menger.c
@@ -0,0 +1,50 @@
+#include "menger.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * is_empty - Checks if the current position should be empty
+ *
+ * @row: Row index
+ * @col: Column index
+ *
+ * Return: 1 if the position should be empty, 0 otherwise
+ */
+int is_empty(int row, int col)
+{
+    while (row > 0 || col > 0)
+    {
+        if (row % 3 == 1 && col % 3 == 1)
+            return (1);
+        row /= 3;
+        col /= 3;
+    }
+    return (0);
+}
+
+/**
+ * menger - Draws a 2D Menger Sponge
+ *
+ * @level: The level of the sponge
+ */
+void menger(int level)
+{
+    int size, row, col;
+
+    if (level < 0)
+        return;
+
+    size = pow(3, level);
+
+    for (row = 0; row < size; row++)
+    {
+        for (col = 0; col < size; col++)
+        {
+            if (is_empty(row, col))
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
+}
