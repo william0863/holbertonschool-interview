fdc7cb2b21f6a48eee72199892ef835fd6af676a
William David <William@w0863.local>
2025-01-31 10:49:19 +0100

    edit pep8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -10,14 +10,11 @@ def minOperations(n):
 
     if n < 2:
         return 0
-    
     operations = 0
     divisor = 2
-    
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
     return operations
