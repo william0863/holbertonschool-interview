a5b6e5b37c5290b5c5fa65f2c0ae9ca9c6859cb2
W <granger.w35@gmail.com>
2025-02-02 22:23:27 +0100

    Update betty style

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -8,29 +8,29 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow, *fast;
+	listint_t *slow;
+	listint_t *fast;
 
-    if (!head)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    slow = head;
-    fast = head;
+	slow = head;
+	fast = head;
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
 
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (slow);
-        }
-    }
-    return (NULL);
+	return (NULL);
 }
