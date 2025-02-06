6613e7aba57cd340f3d0c3ab0034ceca505739d1
W <granger.w35@gmail.com>
2025-02-06 18:06:56 +0100

    Update 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,53 +1,45 @@
 #!/usr/bin/python3
 
-"""Log Metrics Script"""
+""" script that reads stdin line by line and computes metrics """
 
 import sys
-import signal
 
 
-def print_stats():
-    """Prints the accumulated statistics."""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-            
+def printsts(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-total_size = 0
-line_count = 0
 
-def signal_handler(sig, frame):
-    """Handles SIGINT (CTRL+C) and prints statistics before exiting."""
-    print_stats()
-    sys.exit(0)
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-signal.signal(signal.SIGINT, signal_handler)
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        parts = line.split()
-        
-        if len(parts) < 9:
-            continue
-        
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
         try:
-            status_code = int(parts[-2])
-            file_size = int(parts[-1])
-        except ValueError:
-            continue
-        
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-        
-        total_size += file_size
-        line_count += 1
-        
-        if line_count % 10 == 0:
-            print_stats()
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
 
 except KeyboardInterrupt:
-    print_stats()
-    sys.exit(0)
-
+    printsts(sts, size)
+    raise
