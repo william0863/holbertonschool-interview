04149d7715826a95bd006affedec618b57bfb31e
W <granger.w35@gmail.com>
2025-02-06 18:09:19 +0100

    Update 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
-
-""" script that reads stdin line by line and computes metrics """
+"""Log Metrics Script"""
 
 import sys
+import signal
 
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_size = 0
+line_count = 0
 
-def printsts(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+def print_stats():
+    """Prints the accumulated statistics."""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
+def signal_handler(sig, frame):
+    """Handles SIGINT (CTRL+C) and prints statistics before exiting."""
+    print_stats()
+    sys.exit(0)
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+signal.signal(signal.SIGINT, signal_handler)
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
+        parts = line.split()
+        
+        if len(parts) < 9:
+            continue
+        
         try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
+            status_code = int(parts[-2])
+            file_size = int(parts[-1])
+        except ValueError:
+            continue
+        
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        
+        total_size += file_size
+        line_count += 1
+        
+        if line_count % 10 == 0:
+            print_stats()
 
 except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    print_stats()
+    sys.exit(0)
