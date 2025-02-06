48edd5bd948844713bd56bdc4bd3a9e1d2142548
william0863 <granger.w35@gmail.com>
2025-02-06 15:59:53 +0100

    task0

---
+++ merge_sort/0-merge_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * merge - Merges two sorted subarrays into one sorted array
+ * @array: The original array
+ * @temp: Temporary array for merging
+ * @left: Left index
+ * @mid: Middle index
+ * @right: Right index
+ */
+void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
+{
+    size_t i = left, j = mid, k = left;
+
+    printf("Merging...\n[left]: ");
+    print_array(array + left, mid - left);
+    printf("[right]: ");
+    print_array(array + mid, right - mid);
+
+    while (i < mid && j < right)
+    {
+        if (array[i] <= array[j])
+            temp[k++] = array[i++];
+        else
+            temp[k++] = array[j++];
+    }
+
+    while (i < mid)
+        temp[k++] = array[i++];
+    while (j < right)
+        temp[k++] = array[j++];
+
+    for (i = left; i < right; i++)
+        array[i] = temp[i];
+
+    printf("[Done]: ");
+    print_array(array + left, right - left);
+}
+
+/**
+ * merge_sort_recursive - Recursively sorts an array using merge sort
+ * @array: The array to be sorted
+ * @temp: Temporary array for merging
+ * @left: Left index
+ * @right: Right index
+ */
+void merge_sort_recursive(int *array, int *temp, size_t left, size_t right)
+{
+    if (right - left < 2)
+        return;
+
+    size_t mid = left + (right - left) / 2;
+    merge_sort_recursive(array, temp, left, mid);
+    merge_sort_recursive(array, temp, mid, right);
+    merge(array, temp, left, mid, right);
+}
+
+/**
+ * merge_sort - Sorts an array of integers in ascending order using Merge Sort
+ * @array: The array to be sorted
+ * @size: Size of the array
+ */
+void merge_sort(int *array, size_t size)
+{
+    if (size < 2)
+        return;
+
+    int *temp = malloc(size * sizeof(int));
+    if (!temp)
+        return;
+
+    merge_sort_recursive(array, temp, 0, size);
+    free(temp);
+}
+
