204ba652c88aa89134842acb88d599e3b9779bb2
W <granger.w35@gmail.com>
2025-02-06 17:30:25 +0100

    Update 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Log Metrics Script
 
@@ -12,6 +13,7 @@ Expected log format:
 
 import sys
 import signal
+
 
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 total_size = 0
