1c4d0f4a2ca49d08e2269761d43c3e8e218f8019
william0863 <granger.w35@gmail.com>
2025-02-22 16:04:52 +0100

    task0

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,58 @@
+#include "list.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - new node to the end of list
+ * @list: list
+ * @str: string to copy
+ *
+ * Return: new node
+ */
+List *add_node_end(List **list, char *str)
+{
+    List *new_node, *tail;
+
+    if (!list || !str)
+        return (NULL);
+    new_node = malloc(sizeof(List));
+    if (!new_node)
+        return (NULL);
+    new_node->str = strdup(str);
+    if (!new_node->str)
+    {
+        free(new_node);
+        return (NULL);
+    }
+    if (!*list)
+    {
+        new_node->next = new_node;
+        new_node->prev = new_node;
+        *list = new_node;
+    }
+    else
+    {
+        tail = (*list)->prev;
+        tail->next = new_node;
+        new_node->prev = tail;
+        new_node->next = *list;
+        (*list)->prev = new_node;
+    }
+    return (new_node);
+}
+
+/**
+ * add_node_begin - new node to the beginning of list
+ * @list: list
+ * @str: string to copy in new node
+ *
+ * Return: new node
+ */
+List *add_node_begin(List **list, char *str)
+{
+    List *new_node = add_node_end(list, str);
+    if (new_node)
+        *list = new_node;
+    return (new_node);
+}
+
---
+++ double_circular_linked_list/list.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stddef.h>
+
+/**
+ * struct List - doubly linked list
+ * @str: string
+ * @prev: previous node
+ * @next: next node
+ *
+ * Description: doubly linked list
+ *
+ */
+typedef struct List
+{
+    char *str;
+    struct List *prev;
+    struct List *next;
+} List;
+
+List *add_node_end(List **list, char *str);
+List *add_node_begin(List **list, char *str);
+
+#endif /* LIST_H */
+
