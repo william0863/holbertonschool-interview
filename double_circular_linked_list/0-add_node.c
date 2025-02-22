c5f131fe73f2a5dc3e642d66bb67b5318022b1bc
W <granger.w35@gmail.com>
2025-02-22 16:07:25 +0100

    Update betty style

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -11,34 +11,38 @@
  */
 List *add_node_end(List **list, char *str)
 {
-    List *new_node, *tail;
+	List *new_node, *tail;
 
-    if (!list || !str)
-        return (NULL);
-    new_node = malloc(sizeof(List));
-    if (!new_node)
-        return (NULL);
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    if (!*list)
-    {
-        new_node->next = new_node;
-        new_node->prev = new_node;
-        *list = new_node;
-    }
-    else
-    {
-        tail = (*list)->prev;
-        tail->next = new_node;
-        new_node->prev = tail;
-        new_node->next = *list;
-        (*list)->prev = new_node;
-    }
-    return (new_node);
+	if (!list || !str)
+		return (NULL);
+
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	if (!*list)
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		tail = (*list)->prev;
+		tail->next = new_node;
+		new_node->prev = tail;
+		new_node->next = *list;
+		(*list)->prev = new_node;
+	}
+
+	return (new_node);
 }
 
 /**
@@ -50,9 +54,11 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-    List *new_node = add_node_end(list, str);
-    if (new_node)
-        *list = new_node;
-    return (new_node);
-}
+	List *new_node;
 
+	new_node = add_node_end(list, str);
+	if (new_node)
+		*list = new_node;
+
+	return (new_node);
+}
