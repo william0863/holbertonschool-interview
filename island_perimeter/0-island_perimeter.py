62e6d988cd1ae173ff96cb70b4f6a7c558b9cff1
william0863 <granger.w35@gmail.com>
2025-03-09 22:28:29 +0100

    task0

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module containing the function island_perimeter
+that calculates the perimeter of an island in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+
+    Args:
+        grid (list of list of int): The grid representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    height = len(grid)
+    width = len(grid[0]) if height else 0
+    perimeter = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
