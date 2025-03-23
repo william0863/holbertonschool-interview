fc4ce08cdad7b21530c71ef8096ce7bb3df148d5
william0863 <granger.w35@gmail.com>
2025-03-23 19:04:18 +0100

    task0

---
+++ making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
