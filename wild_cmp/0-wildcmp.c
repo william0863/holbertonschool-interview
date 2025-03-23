56a2cbd00c45a0fe4181d83febe4f30d2dbcc647
william0863 <granger.w35@gmail.com>
2025-03-23 18:17:15 +0100

    task0

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,31 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int wildcmp(char *s1, char *s2);
+
+#endif /* HOLBERTON_H */
+
+/**
+ * wildcmp - Compares two strings allowing for wildcard * in s2
+ * @s1: The first string
+ * @s2: The second string, which may contain '*'
+ *
+ * Return: 1 if the strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+    if (*s2 == '*')
+    {
+        if (!*s1)
+            return (wildcmp(s1, s2 + 1));
+        return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+    }
+    if (*s1 == *s2)
+    {
+        if (!*s1)
+            return (1);
+        return (wildcmp(s1 + 1, s2 + 1));
+    }
+    return (0);
+}
+
