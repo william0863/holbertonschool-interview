cd10bf021929d90dbb45415161224468790bc899
W <granger.w35@gmail.com>
2025-04-13 22:00:30 +0200

    Update substring.h

--- substring/substring.h
+++ substring/substring.h
@@ -2,6 +2,6 @@
 #define SUBSTRING_H
 
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
-int *process_substring(char const *s, char const **words, int nb_words, int i, int word_len);
+int process_substring(char const *s, char const **words, int nb_words, int i, int word_len);
 
 #endif
